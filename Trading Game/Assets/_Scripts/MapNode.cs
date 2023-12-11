228dffd88ef183dae69fcfaea4f14e546334034f
Dommapomma <dominic.x.mao@gmail.com>
2023-12-11 15:19:37 -0700

    broken rn

--- Trading Game/Assets/_Scripts/MapNode.cs
+++ Trading Game/Assets/_Scripts/MapNode.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class MapNode : MonoBehaviour
 {
+    [SerializeField] private MapNode mapNode;
+    [SerializeField] private int generation = 0;
+
     private void Start()
     {
-        
+        if (generation <= 5)
+        {
+            int childNodes = Random.Range(1, 3);
+            for (int i = 0; i < childNodes; i++)
+            {
+                MapNode node = Instantiate(mapNode, this.transform);
+                node.SetGeneration(generation++);
+            }
+        }
+    }
+    public void SetGeneration(int gen)
+    {
+        generation = gen;
     }
 }
--- Trading Game/Assets/_Scripts/_UI/MapMenuUI.cs
+++ Trading Game/Assets/_Scripts/_UI/MapMenuUI.cs
@@ -12,7 +12,7 @@ public class MapMenuUI : MonoBehaviour
     [SerializeField] private BaseEnemy fireEnemy;
     [SerializeField] private BaseEnemy poisonEnemy;
 
-    [SerializeField] private int seed = 0;
+
     private void Awake()
     {
         poisonButton.onClick.AddListener(() =>
@@ -33,23 +33,22 @@ public class MapMenuUI : MonoBehaviour
         EnemyType.enemyType = poisonEnemy;
     }
 
-    [SerializeField]
+    [SerializeField] private int seed = 0;
+    [SerializeField] private MapNode mapNode;
 
     private void GenerateMap()
     {
         SetSeed();
-        //Instantiate(Map)
+        Instantiate(mapNode, this.transform);
 
     }
     private void SetSeed()
     {
         if (seed == 0)
         {
-            seed = Random.Range(1000000, 9999999);     
+            seed = Random.Range(1000000, 9999999);
         }
         Random.InitState(seed);
     }
-
-
 
 }
